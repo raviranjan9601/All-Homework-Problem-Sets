cffa526cd170fb3831fab7367c86df0c288d03d3
Ravi Ranjan <raviranjan9601@gmail.com>
2025-12-28 00:26:17 +0530

    variable

---
+++ variable.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    int munber = 25;
+    int age = 21;
+    float height = 5.11;
+    float pi = 3.14;
+    float weight = 65.7;
+    
+    // variable name are case sensitive 
+    
+    int Num = 55;
+    int Num1 = 56;
+    char _hastag = "#";
+    char _and = "&";
+    char _asterisk = "*";
+
+    //1st character is alphabet ot "_"
+    
+    int Final_price = 100;
+    int last_price = 90;
+
+    // no comma no blank space
+    
+    char _hastag = "#";
+    char _and = "&";
+    char _asterisk = "*";
+
+    // no other symbole other then "_"
+
+    // variable value are changable like 
+    
+    int now_speed = 150;
+    now_speed = 165;
+
+    // variable name alwayes meaning full 
+    // like : age, height, hastag and final_price
+
+
+}
+
