554ba345bbd09b87503663e2285ca4ea5188bcae
Ravi Ranjan <raviranjan9601@gmail.com>
2025-12-28 15:58:46 +0530

    add new

---
+++ input.c
@@ -0,0 +1,91 @@
+// input function are use to take data frome user. 
+
+
+#include <stdio.h>
+
+int main(){
+    
+    //int num1, num2;
+
+    //printf("Enter 1st number for sum: ");
+    //scanf("%d",&num1);
+
+    //printf("Enter 2nd num for sum: ");
+    //scanf("%d", &num2);
+
+    //int sum = num1 + num2;
+
+    //printf("%d\n",sum);
+    
+    //int s1, s2;
+    
+    //printf("enter num S1: ");
+    //scanf("%d", &s1);
+
+    //printf("enter num S2: ");
+    //scanf("%d", &s2);
+    
+    //int subtract = s1 - s2;
+    
+    //printf("%d\n", subtract);
+    
+    
+    //int div1, div2;
+    
+    //printf("enter div1: ");
+    //scanf("%d", &div1);
+    
+    //printf("Enter div2: ");
+    //scanf("%d", &div2);
+    
+    //int division = div1 / div2;
+    
+    //printf("%d\n", division);
+    
+    int multi1, multi2;
+    
+    printf("enter 1st num");
+    scanf("%d", &multi1);
+    
+    printf("enter 2nd num");
+    scanf("%d", &multi2);
+    
+    int multiplie = multi1 * multi2;
+    
+    printf("%d", multiplie);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    
+    getchar();
+    char name[50];
+    long long Enrollment;
+    
+    // now we design a small registration form useing scanf 
+    printf("FRESHER PARTY REGISTRATION FORM\n");
+
+    printf("Enter Your Name : ");
+    fgets(name, 50, stdin);
+
+
+    printf("Enter Your Enrollment Number: ");
+    scanf("%lld", &Enrollment);
+    
+    printf("your enrollment no. is %lld", Enrollment);*/
+    
+    return 0;
+    
+}
